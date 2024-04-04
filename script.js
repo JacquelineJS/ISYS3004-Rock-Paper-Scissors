20f4a7ad49aa1b4a3d547abfdfbbeace2ab8267b
JacquelineJS <127270991+JacquelineJS@users.noreply.github.com>
2024-04-04 14:35:55 +0800

    add a score and text to speech

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* Lets keep score! */
+let win = 0;
+let lose = 0;
+let tie = 0;
+
+function say(theMessage) {
+    let msg = new SpeechSynthesisUtterance(theMessage);
+    speechSynthesis.speak(msg);
+}
+
 
 /* This is a simplified version of the game.
 The Computer ALWAYS picks ROCK.  
@@ -6,12 +16,21 @@ function play(user) {
     let result = "";
     if (user === 'rock') {
         result = "tie";
+        tie++;
     }
     if (user === 'paper') {
         result = "win";
+        win++;
     }
     if (user === 'scissors') {
         result = "lose";
+        lose++;
     }
-    alert("Computer chose rock, you " + result + "!");
+    let myMessage = ("Computer chose rock, you " + result + "!" +
+    "\n\n" + "win" + "win" +
+    "\n" + loss + "loss" +
+    "\n" + tie +"tie");
+    alert(myMessage)
+    say(myMessage)
+
 }
